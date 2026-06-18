f6603dd57ccab4d9b7ba52e61e0c9a2180f5f9d2
JASMINEJOYS <144472155+JASMINEJOYS@users.noreply.github.com>
2026-06-18 11:36:02 +0530

    Update number.py

--- number.py
+++ number.py
@@ -12,4 +12,20 @@
 # else:
 #     print(f"{num} is odd number")
 
+# letters=input("Enter an Alphabet:")
+
+# num=int(input("Enter a number:"))
+# if num<10 and num>-10:
+#     print(f"{num} is a Single digit")
+# else:
+#     print(f"{num} is a multi digit number")
+
+# vowels=["a","e","i","o","u"]
+# letter=input("Enter an alphabet:").lower()
+
+# if letter in vowels:
+#     print(f"{letter} is a vowel")
+# else:
+#     print(f"{letter} is a consonant")
+
 
